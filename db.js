a1c68af73e9891d535f2c386b46ca68eb5884520
Naren <76785483+batarang24@users.noreply.github.com>
2025-03-11 12:31:27 +0530

    Updating hosts...

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'faqs_user', // Change as needed
     password: 'chandiran5', // Change as needed
     database: 'faqs',
     port:3306
-});*/
+});
 
 db.connect(err => {
     if (err) {
