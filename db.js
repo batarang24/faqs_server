81d8df49eb5de12c2838a85c4552eb12844ded0b
Naren <76785483+batarang24@users.noreply.github.com>
2025-03-11 12:25:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'faqs_user', // Change as needed
     password: 'chandiran5', // Change as needed
     database: 'faqs',
